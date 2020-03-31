1a32b45a5d6d1510c39fd9dff5ca104f5a2e2036
Philipp Preß <ntraum@fastmail.com>
2020-03-31 21:53:44 +0200

    RSpec: Do not fail fast

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require 'sinatra/activerecord'
 RSpec.configure do |config|
   config.disable_monkey_patching!
 
-  config.fail_fast = true
-
   config.before { FileUtils.mkdir_p("tmp") }
   config.after  { FileUtils.rm_rf("tmp") }
 end
