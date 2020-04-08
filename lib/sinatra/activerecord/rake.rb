84dae2d193ae6424ce318171ee8e2c9675b90aa2
Philipp Preß <ntraum@fastmail.com>
2020-04-08 18:32:22 +0200

    Do not overwrite ActiveRecord logger

--- lib/sinatra/activerecord/rake.rb
+++ lib/sinatra/activerecord/rake.rb
@@ -6,5 +6,3 @@ load 'active_record/railties/databases.rake'
 require "sinatra/activerecord/rake/activerecord_#{ActiveRecord::VERSION::MAJOR}"
 
 load 'sinatra/activerecord/tasks.rake'
-
-ActiveRecord::Base.logger = nil
