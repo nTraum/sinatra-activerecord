4521698bc95b4cbb9e3386a02ffd5843ccf2c215
Philipp Preß <ntraum@fastmail.com>
2020-04-10 01:37:12 +0200

    Add specs for all sane database configurations

--- lib/sinatra/activerecord.rb
+++ lib/sinatra/activerecord.rb
@@ -15,6 +15,13 @@ module Sinatra
   end
 
   module ActiveRecord
+    DATABASE_FILE_RELATIVE_ERROR_MESSAGE = <<~MSG
+      database_file must not be relative when root is not defined.
+      Change database_file to an absolute path or set the app root of your sinatra app instead.
+    MSG
+
+    # Registers the extension for the specified app.
+    # @param app [Sinatra::Base] The sinatra app.
     def self.registered(app)
       if ENV['DATABASE_URL']
         app.set :database, ENV['DATABASE_URL']
@@ -28,16 +35,22 @@ module Sinatra
     end
 
     def database_file=(path)
-      path = File.join(root, path) if Pathname(path).relative? && root
+      if Pathname(path).relative?
+        raise(ArgumentError, DATABASE_FILE_RELATIVE_ERROR_MESSAGE) unless root
+
+        path = File.join(root, path)
+      end
       spec = YAML.safe_load(ERB.new(File.read(path)).result) || {}
       set :database, spec
     end
 
     def database=(spec)
+      # with environment?
       if spec.is_a?(Hash) && spec.symbolize_keys[environment.to_sym]
         ::ActiveRecord::Base.configurations = spec.stringify_keys
         ::ActiveRecord::Base.establish_connection(environment.to_sym)
       elsif spec.is_a?(Hash)
+        # without environment?
         ::ActiveRecord::Base.configurations = { environment.to_sym => spec }
         ::ActiveRecord::Base.establish_connection(spec.stringify_keys)
       else
--- spec/sinatra/activerecord_spec.rb
+++ spec/sinatra/activerecord_spec.rb
@@ -1,102 +1,215 @@
 # frozen_string_literal: true
 
+require 'active_support/core_ext/hash/keys'
 require 'fileutils'
+require 'pathname'
+
+RSpec.shared_context 'when database.yml exists' do
+  let(:database_file) { 'config/database.yml' }
+  let(:database_spec) { spec_for(envs: [:test]) }
+
+  before do
+    FileUtils.mkdir_p(Pathname.new(database_file).dirname)
+    File.write(database_file, database_spec.to_yaml)
+  end
+
+  # @param envs [Array<Symbol>] The envs that will end up in the spec.
+  # @return [Hash, nil] Spec hash including all specified environments. Returns `nil` if envs is empty.
+  def spec_for(envs: [])
+    envs.map do |env|
+      { env.to_s => { 'adapter' => 'sqlite3', 'database' => "db/#{env}.sqlite3" } }
+    end.reduce(:merge)
+  end
+end
+
+RSpec.shared_context 'when DATABASE_URL exists' do
+  let!(:database_url) { 'sqlite3://tmp/foo.sqlite3' }
+
+  around(:each) do |example|
+    begin
+      ENV['DATABASE_URL'] = database_url
+      example.run
+    ensure
+      ENV.delete('DATABASE_URL')
+    end
+  end
+end
+
+RSpec.shared_examples 'connects to the database' do
+  it 'does not raise' do
+    expect { subject }.not_to raise_error
+  end
+end
+
+RSpec.shared_examples 'raises error when connecting to the database' do
+  it 'raises' do
+    expect { subject }.to raise_error
+  end
+end
 
 RSpec.describe Sinatra::ActiveRecord do
-  let(:app) do
+  subject(:app) do
     Class.new(Sinatra::Base) do
       set :root, nil
       register Sinatra::ActiveRecord
     end
   end
 
-  describe 'database spec' do
-    context 'when DATABASE_URL is given' do
-      let!(:database_url) { 'sqlite3:///tmp/foo.sqlite3' }
+  around(:each) do |example|
+    within_isolated_app_dir(create_app_files: false, &example)
+  end
 
-      around(:each) do |example|
-        begin
-          ENV['DATABASE_URL'] = database_url
-          example.run
-        ensure
-          ENV.delete('DATABASE_URL')
-        end
+  describe 'loading database configuration when included' do
+    context 'only DATABASE_URL' do
+      include_context 'when DATABASE_URL exists'
+      include_examples 'connects to the database'
+    end
+
+    context 'only database.yml' do
+      include_context 'when database.yml exists'
+      context 'single database' do
+        let(:database_spec) { spec_for(envs: [:test]) }
+        include_examples 'connects to the database'
       end
 
-      it 'establishes the connection with a database url' do
-        app
-        expect { ActiveRecord::Base.connection }.not_to raise_error
+      context 'invalid (empty) spec' do
+        let(:database_spec) { {} }
+        include_examples 'raises error when connecting to the database'
+      end
+
+      context 'multiple databases' do
+        let(:database_spec) { spec_for(envs: %i[development test]) }
+
+        it 'connects to both databases' do
+          app
+          expect { ActiveRecord::Base.establish_connection(:test) }.not_to raise_error
+          expect { ActiveRecord::Base.establish_connection(:development) }.not_to raise_error
+        end
       end
     end
   end
 
-  it 'exposes ActiveRecord::Base' do
-    expect(app.database).to eq ActiveRecord::Base
+  describe '#database_file=' do
+    subject do
+      app.root = root
+      app.database_file = database_file
+    end
+
+    context 'invalid (empty) file' do
+      include_context 'when database.yml exists'
+      let(:database_file) { 'config/my_db.yml' }
+      let(:database_spec) { {} }
+      include_examples 'raises error when connecting to the database'
+    end
+
+    context 'file does not exist' do
+      let(:database_file) { 'config/my_db.yml' }
+      include_examples 'raises error when connecting to the database'
+    end
+
+    context '#root is absolute' do
+      include_context 'when database.yml exists'
+      let(:root) { Dir.pwd }
+
+      context '#database_file is absolute' do
+        let(:database_file) { File.join(root, 'config/my_db.yml') }
+        include_examples 'connects to the database'
+      end
+
+      context '#database_file is relative' do
+        let(:database_file) { 'config/my_db.yml' }
+        include_examples 'connects to the database'
+      end
+    end
+
+    context '#root is relative' do
+      include_context 'when database.yml exists'
+      let(:root) { '.' }
+
+      context '#database_file is absolute' do
+        let(:database_file) { File.join(Dir.pwd, 'config/my_db.yml') }
+        include_examples 'connects to the database'
+      end
+
+      context '#database_file is relative' do
+        let(:database_file) { 'config/my_db.yml' }
+        include_examples 'connects to the database'
+      end
+    end
+
+    context '#root is nil' do
+      include_context 'when database.yml exists'
+      let(:root) { nil }
+
+      context '#database_file is absolute' do
+        let(:database_file) { File.join(Dir.pwd, 'config/my_db.yml') }
+        include_examples 'connects to the database'
+      end
+
+      context '#database_file is relative' do
+        let(:database_file) { 'config/my_db.yml' }
+        include_examples 'raises error when connecting to the database'
+      end
+    end
   end
 
-  it 'establishes the connection with a hash' do
-    app.database = { adapter: 'sqlite3', database: 'tmp/foo.sqlite3' }
+  describe '#database=' do
+    subject { app.database = database_spec }
 
-    expect { ActiveRecord::Base.connection }.not_to raise_error
+    context 'a hash without environment with string keys' do
+      let(:database_spec) { { adapter: 'sqlite3', database: 'db/foo.sqlite3' }.stringify_keys }
+      include_examples 'connects to the database'
+    end
+
+    context 'a hash without environment with symbol keys' do
+      let(:database_spec) { { adapter: 'sqlite3', database: 'db/foo.sqlite3' } }
+      include_examples 'connects to the database'
+    end
+
+    context 'a hash with environment nesting and symbol keys' do
+      let(:database_spec) { { test: { adapter: 'sqlite3', database: 'db/foo.sqlite3' } } }
+      include_examples 'connects to the database'
+    end
+
+    context 'a hash with environment nesting and string keys' do
+      let(:database_spec) { { test: { adapter: 'sqlite3', database: 'db/foo.sqlite3' } }.stringify_keys }
+      include_examples 'connects to the database'
+    end
+
+    context 'an empty hash' do
+      let(:database_spec) { {} }
+      include_examples 'raises error when connecting to the database'
+    end
+
+    describe 'supports sinatra app env' do
+      let(:database_spec) { { test: { adapter: 'sqlite3', database: 'db/foo.sqlite3' } }.stringify_keys }
+
+      context 'as key' do
+        before { app.environment = :test }
+        include_examples 'connects to the database'
+      end
+
+      context 'as string' do
+        before { app.environment = 'test' }
+        include_examples 'connects to the database'
+      end
+    end
   end
 
-  it 'handles namespacing into environments' do
-    app.environment = :development
-    app.database = { development: { adapter: 'sqlite3', database: 'tmp/foo.sqlite3' } }
+  describe '#database' do
+    subject { app.database }
 
-    expect { ActiveRecord::Base.connection }.not_to raise_error
-  end
+    context 'when valid database config exists' do
+      include_context 'when DATABASE_URL exists'
+      it 'returns ActiveRecord::Base' do
+        expect(subject).to eq(ActiveRecord::Base)
+      end
+    end
 
-  it 'allows settings environments as Strings' do
-    app.environment = 'development'
-    app.database = { development: { adapter: 'sqlite3', database: 'tmp/foo.sqlite3' } }
-
-    expect { ActiveRecord::Base.connection }.not_to raise_error
-  end
-
-  it 'raises an appropriate error when the database spec is invalid' do
-    expect { app.database = {} }.to raise_error(ActiveRecord::AdapterNotSpecified)
-  end
-
-  it "doesn't try to establish connection when database isn't set" do
-    expect { app.database }.not_to raise_error
-  end
-
-  it 'allows specifying database through a file' do
-    app.database_file = 'spec/fixtures/database.yml'
-
-    expect { ActiveRecord::Base.connection }.not_to raise_error
-  end
-
-  it 'allows specifying multiple databases' do
-    app.root = 'spec/fixtures'
-    app.database_file = 'database.yml'
-
-    expect { ActiveRecord::Base.establish_connection(:test) }.not_to raise_error
-    expect { ActiveRecord::Base.establish_connection(:another_test) }.not_to raise_error
-  end
-
-  it 'expands database file path from the app root if present' do
-    app.root = 'spec/fixtures'
-    app.database_file = 'database.yml'
-
-    expect { ActiveRecord::Base.connection }.not_to raise_error
-  end
-
-  it "doesn't expand the database file path from the app root if the path is absolute" do
-    app.root = 'spec/fixtures'
-    app.database_file = "#{Dir.pwd}/spec/fixtures/database.yml"
-
-    expect { ActiveRecord::Base.connection }.not_to raise_error
-  end
-
-  it 'raises an error on invalid database.yml' do
-    FileUtils.touch('tmp/database.yml')
-
-    expect { app.database_file = 'tmp/database.yml' }.to raise_error(ActiveRecord::AdapterNotSpecified)
-  end
-
-  it 'raises an error on missing database.yml' do
-    expect { app.database_file = 'foo.yml' }.to raise_error(Errno::ENOENT)
+    context 'when there is no database config' do
+      # TODO: Shouldn't this be nil
+      # Why is there a database configuration
+      xit { is_expected.to eq(nil) }
+    end
   end
 end
