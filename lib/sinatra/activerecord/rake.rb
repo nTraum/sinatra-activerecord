a64ea181dc8741828d57eff602f0d3f57436a7ac
Philipp Preß <ntraum@fastmail.com>
2020-04-10 03:00:01 +0200

    Further tries

--- lib/sinatra/activerecord/rake.rb
+++ lib/sinatra/activerecord/rake.rb
@@ -3,6 +3,8 @@
 require 'rake'
 
 load 'active_record/railties/databases.rake'
+
+require "sinatra/activerecord"
 require "sinatra/activerecord/rake/activerecord_#{ActiveRecord::VERSION::MAJOR}"
 
 load 'sinatra/activerecord/tasks.rake'
