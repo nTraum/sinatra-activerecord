1d5048030b72c6d471d29eefc9b2ca902550d30d
Philipp Preß <ntraum@fastmail.com>
2020-04-14 16:59:57 +0200

    clean up rake integration

--- lib/sinatra/activerecord/rake.rb
+++ lib/sinatra/activerecord/rake.rb
@@ -4,7 +4,23 @@ require 'rake'
 
 load 'active_record/railties/databases.rake'
 
-require 'sinatra/activerecord'
-require "sinatra/activerecord/rake/activerecord_#{ActiveRecord::VERSION::MAJOR}"
+require_relative "rake/activerecord_#{ActiveRecord::VERSION::MAJOR}"
 
-load 'sinatra/activerecord/tasks.rake'
+require_relative 'tasks/migration_creator.rb'
+
+namespace :db do
+  desc 'Create a migration (parameters: NAME, VERSION)'
+  task :create_migration, [:name] do |_task, args|
+    migration_creator = Sinatra::ActiveRecord::Tasks::MigrationCreator.new(args: args.to_a)
+    migration_creator.run
+  end
+end
+
+# The `db:create` and `db:drop` command won't work with a DATABASE_URL because
+# the `db:load_config` command tries to connect to the DATABASE_URL, which either
+# doesn't exist or isn't able to drop the database. Ignore loading the configs for
+# these tasks if a `DATABASE_URL` is present.
+if ENV.key? 'DATABASE_URL'
+  Rake::Task['db:create'].prerequisites.delete('load_config')
+  Rake::Task['db:drop'].prerequisites.delete('load_config')
+end
